66fa8cd546de66208472bdc9711ba0be5e313673
Nic Nilov <nicnilov@gmail.com>
2020-03-17 13:59:59 +0200

    Zsh updates

--- shell/custom.zsh
+++ shell/custom.zsh
@@ -4,23 +4,31 @@
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
 #
 [[ -z $XDG_CONFIG_HOME ]] && export XDG_CONFIG_HOME="$HOME/.config"
+[[ -z $GOPATH ]] && export GOPATH="$HOME/projects/go"
 
 export EDITOR='vim'
 setopt nosharehistory
-source ~/dotfiles/tmuxinator/tmuxinator.zsh
 
-alias ls='ls --color=auto'
-alias ll='ls -la -v'
-alias -g gp='| grep -i'
+alias l='LC_COLLATE=C gls -lah -v --time-style="+%Y-%m-%d %H:%M:%S" --group-directories-first --color=auto' # depends on `brew install coreutils`
+alias ll='LC_COLLATE=C gls -la -v --time-style="+%Y-%m-%d %H:%M:%S" --group-directories-first --color=auto' # depends on `brew install coreutils`
+alias -g grp='| grep -i'
 alias -s rb=vim
 alias -s txt=vim
 alias -s lg="less -MN"
 alias rc='bundle exec rails c'
 alias hrc='heroku run rails c'
 alias rs='bundle exec rails s'
+alias hg="history | grep"
 alias hist='history'
 alias ber='bundle exec rspec'
+alias git=g
+alias dm=docker-machine
+alias iem="iex -S mix"
+alias mtx="iex -S mix test --trace"
+alias exifstrip="exiftool -all="
+alias ic=imgcat
+alias ils=imgls
 
-source ~/.profile
+# source ~/.profile
 
-eval `dircolors ~/dotfiles/shell/.dircolors`
+# eval `dircolors ~/dotfiles/shell/.dircolors`
